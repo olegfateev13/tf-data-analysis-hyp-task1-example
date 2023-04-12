631412479ad921af2d95d8e39a75d74ae04ad4a8
olegfateev13 <58751132+olegfateev13@users.noreply.github.com>
2023-04-12 21:27:27 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,12 +12,22 @@ def solution(x_success: int,
     alpha = 0.1
     conversion_control = x_success / x_cnt
     conversion_test = y_success / y_cnt
+
+    # Рассчет стандартной ошибки для контроля и теста
     se_control = np.sqrt(conversion_control * (1 - conversion_control) / x_cnt)
     se_test = np.sqrt(conversion_test * (1 - conversion_test) / y_cnt)
+
+    # Рассчет Z-статистики
     z_stat = (conversion_test - conversion_control) / np.sqrt(se_control**2 + se_test**2)
-    p_value = stats.norm.sf((z_stat))
+
+    # Рассчет p-значения
+    p_value = stats.norm.sf(abs(z_stat))
+
+
+    # Принятие решения об отклонении нулевой гипотезы
     if p_value < alpha:
-        decision = True 
+        decision = True # Отклоняем нулевую гипотезу
     else:
-        decision = False 
-    return decision 
+        decision = False # Принимаем нулевую гипотезу 
+    
+    return decision # Ваш ответ, True или False
